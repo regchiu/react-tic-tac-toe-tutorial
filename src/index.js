c82c5936d4b5e052babf09f96b09dbf64cf8ef30
regchiu <regx1385@gmail.com>
2022-05-09 14:55:01 +0800

    feat: Display the location for each move in the format (col, row) in the move history list

--- src/index.js
+++ src/index.js
@@ -38,6 +38,8 @@ class Board extends React.Component {
     );
   }
 
+  square
+
   render() {
     return (
       <div>
@@ -74,6 +76,17 @@ class Game extends React.Component {
   }
 
   handleClick(i) {
+    const locations = [
+      [1, 1],
+      [2, 1],
+      [3, 1],
+      [1, 2],
+      [2, 2],
+      [3, 2],
+      [1, 3],
+      [2, 3],
+      [3, 3]
+    ];
     const history = this.state.history.slice(0, this.state.stepNumber + 1);
     const current = history[history.length - 1];
     const squares = current.squares.slice();
@@ -84,6 +97,7 @@ class Game extends React.Component {
     this.setState({
       history: history.concat([{
         squares: squares,
+        location: locations[i]
       }]),
       stepNumber: history.length,
       xIsNext: !this.state.xIsNext
@@ -103,7 +117,7 @@ class Game extends React.Component {
     const winner = calculateWinner(current.squares);
     const moves = history.map((step, move) => {
       const desc = move ?
-        'Go to move #' + move :
+        'Go to move #' + move + " @ " + history[move].location:
         'Go to game start';
       return (
         <li key={move}>
